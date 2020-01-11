7fe86c9733612a6c67c4a0ba992ebd5354043ee0
oalvher <oalvher95@gmail.com>
2020-01-11 11:33:51 +0000

    PEC5
    > Angular universal y despliegue en Github

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { rootRouterConfig } from './app-routing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { environment } from '../environments/environment';
     CoreModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
     BrowserAnimationsModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   declarations: [AppComponent],
